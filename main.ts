a4e2640d7f68f3436183557f23ac5d67dbbe0906
abdulsamimeo34 <160492688+abdulsamimeo34@users.noreply.github.com>
2024-04-01 00:33:24 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,16 @@
+import inquirer from "inquirer";
+let mybalance = 10000; // dollar
+let mypin = 3333;
+
+let pinanswer = inquirer.prompt(
+    [
+    {
+        name : "q1",
+        message:"enter your pin",
+        type:"number"
+    }
+
+    ]
+)
+
+console.log(mybalance);
